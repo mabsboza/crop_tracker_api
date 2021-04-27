49ece7c40c0ba5b8040dfc0de08573cb48abbdcb
mabsboza <marcosboza@moi-solutions.com>
2021-04-27 00:38:44 -0600

    Add code line for prod

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors())
-
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 app.get('/', function (req, res) {
-res.send('Express application working ...');
+    res.send('Express application working ...');
 });
 // Start server
 app.listen(8080, () => console.log('server running ...'))
